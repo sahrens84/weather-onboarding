f546856491b24a7b99af53c4a7c819eb91ab8cc0
sahrens84 <sahrens84>
2024-04-25 09:55:04 +0000

    store transformed data in silver layer

--- silver/weatherdotcom/transform.py
+++ silver/weatherdotcom/transform.py
@@ -1,2 +1,54 @@
 # Databricks notebook source
 df  = spark.read.json("/Volumes/prod/bronze/file_storage/weatherdotcom/25042024/")
+df.createOrReplaceTempView("base")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC WITH data AS (
+# MAGIC   SELECT DISTINCT
+# MAGIC     current.* EXCEPT(condition),
+# MAGIC     current.condition.*,
+# MAGIC   location.* 
+# MAGIC   FROM base
+# MAGIC )
+# MAGIC
+# MAGIC SELECT
+# MAGIC   name As city_name,
+# MAGIC   region,
+# MAGIC   country,
+# MAGIC   tz_id,
+# MAGIC   lat,
+# MAGIC   lon,
+# MAGIC   cloud,
+# MAGIC   feelslike_c,
+# MAGIC   feelslike_f,
+# MAGIC   gust_kph,
+# MAGIC   gust_mph,
+# MAGIC   humidity,
+# MAGIC   is_day::BOOLEAN,
+# MAGIC   last_updated::TIMESTAMP,
+# MAGIC   last_updated_epoch,
+# MAGIC   precip_in,
+# MAGIC   precip_mm,
+# MAGIC   pressure_in,
+# MAGIC   pressure_mb,
+# MAGIC   temp_c,
+# MAGIC   temp_f,
+# MAGIC   uv,
+# MAGIC   vis_km,
+# MAGIC   vis_miles,
+# MAGIC   wind_degree,
+# MAGIC   wind_dir,
+# MAGIC   wind_kph,
+# MAGIC   wind_mph,
+# MAGIC   code,
+# MAGIC   icon,
+# MAGIC   text,
+# MAGIC   localtime::TIMESTAMP,
+# MAGIC   localtime_epoch
+# MAGIC FROM data
+
+# COMMAND ----------
+
+_sqldf.write.mode('overwrite').saveAsTable("prod.silver.city_weather")
