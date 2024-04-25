708f24d819f7c79b917e87d84e2b60c5e152c364
sahrens84 <sahrens84>
2024-04-25 08:08:39 +0000

    add city extraction

---
+++ bronze/weatherdotcom/extract_data.py
@@ -0,0 +1,24 @@
+# Databricks notebook source
+import requests
+from common.utils import store_json_file, os
+from datetime import datetime
+
+# COMMAND ----------
+
+API_KEY = dbutils.secrets.get("weather-api", "api-key")
+BASE_URL = "http://api.weatherapi.com/v1"
+STORAGE_PATH = "/Volumes/prod/bronze/file_storage/"
+
+CITIES = ["London", "Paris", "Berlin", "Tokio"]
+
+now = datetime.now() 
+date = now.strftime("%d%m%Y")
+time = now.strftime("%H%M%S")
+source_system = "weatherdotcom"
+path = os.path.join(STORAGE_PATH, source_system, date)
+
+# COMMAND ----------
+
+for city in CITIES:
+    data = requests.get(f"{BASE_URL}/current.json?key={API_KEY}&q={city}").json()
+    store_json_file(data=data, path=path, file_name=f"{city}")
---
+++ common/utils.py
@@ -0,0 +1,22 @@
+from typing import Optional
+import os 
+import json
+
+def store_json_file(data: str, path: str, file_name: str) -> Optional[str]:
+    """
+    Store extracted JSON data in a file.
+
+    :param
+        data: Content of the json file.
+        path: Storage location, e.g. /Volumes/dev/
+        file_name: Name of the file to be saved.
+    :return: Error message as a string or None if successful.
+    """
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    with open(f"{path}/{file_name}.json", "w", encoding="utf-8") as f:
+        try:
+            json.dump(data, f, ensure_ascii=False, indent=4)
+        except Exception as e:
+            return str(e)
